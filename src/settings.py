3e1e5521e12db3b10425e80ca2e243fbed808bb0
Матвей Баранов <user22@computer12.medbank>
2025-12-24 16:12:28 +0300

    Initial 2

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import Literal
+
 from pydantic import BaseModel, Field
 from pydantic_settings import BaseSettings
 
@@ -8,3 +11,5 @@ class AISettings(BaseModel):
 
 class Settings(BaseSettings):
     ai: AISettings = Field(default_factory=AISettings)
+    launch_type: Literal["CRON"] = "CRON"
+    resource_path: Path = Path("./resources")
